5a6f69644ab00f882f2fca272e48c6fb19babd97
SunwooKim11 <seonu2001@naver.com>
2024-09-29 06:05:43 +0900

    fix: WrongNoteService 에러 수정

--- src/main/kotlin/kokoton/sextet/service/WrongNoteService.kt
+++ src/main/kotlin/kokoton/sextet/service/WrongNoteService.kt
@@ -1,6 +1,7 @@
 package kokoton.sextet.service
 
 import kokoton.sextet.dto.WrongNoteResponseDTO
+import kokoton.sextet.model.SpellingAnswerNote
 import kokoton.sextet.model.SpellingAnswerNoteRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -13,13 +14,13 @@ class WrongNoteService(
     // 오답노트를 받아오는 메서드
     fun getWrongNotes(userId: Long, count: Int): List<WrongNoteResponseDTO> {
         // 사용자가 제출한 답변 중 오답만 찾기, 최신순으로 정렬
-        val wrongNotes = spellingAnswerNoteRepository.findAllByUserId(userId)
-            .filter { note -> note.answer != note.quiz?.answer }
-            .sortedByDescending { it.createdAt }
-            .take(count)  // 요청한 개수만큼 제한, 없으면 최대 개수 50개 리턴
+        val wrongNotes: List<SpellingAnswerNote> = spellingAnswerNoteRepository.findAllByUserId(userId)
+            .filter { note: SpellingAnswerNote -> note.answer != note.quiz?.answer }
+            .sortedByDescending { note: SpellingAnswerNote -> note.createdAt }
+            .take(count)
 
         // 오답노트 리스트 변환
-        return wrongNotes.map { note ->
+        return wrongNotes.map { note: SpellingAnswerNote ->
             WrongNoteResponseDTO(
                 quizId = note.quiz?.id ?: 0,
                 options = note.quiz?.options?.split(",") ?: emptyList(),
