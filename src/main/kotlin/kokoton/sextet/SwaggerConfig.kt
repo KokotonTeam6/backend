9e01a2ec38508036333967ed3704690ede9eba83
dalbodeule <11470513+dalbodeule@users.noreply.github.com>
2024-09-28 23:03:12 +0900

    SwaggerConfig.kt has some change - with http/https

--- src/main/kotlin/kokoton/sextet/SwaggerConfig.kt
+++ src/main/kotlin/kokoton/sextet/SwaggerConfig.kt
@@ -5,6 +5,9 @@ import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.servers.Server
+import jakarta.servlet.ServletContext
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -14,7 +17,7 @@ class SwaggerConfig {
     private val securitySchemeName = "api token"
 
     @Bean
-    fun openAPI(): OpenAPI {
+    fun openAPI(request: ServletContext): OpenAPI {
         return OpenAPI()
             .info(Info().title("코코톤 6팀 Backend RestAPI").version("v1.0.0"))
             .addSecurityItem(SecurityRequirement().addList(securitySchemeName))
@@ -26,5 +29,9 @@ class SwaggerConfig {
                         .scheme("bearer")
                 )
             )
+            .servers(listOf(
+                Server().url(request.contextPath)
+                    .description(request.contextPath)
+            ))
     }
 }
